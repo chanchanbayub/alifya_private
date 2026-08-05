dfcf9103791ce6eb034566560bbcec0e56cc4830
Chan Chan Bayu Bahari <63919358+chanchanbayub@users.noreply.github.com>
2026-08-05 10:55:26 +0700

    Update invoice_mitra_ahl_pdf.php
    update Invoice PDF

--- app/Views/pdf/invoice_mitra_ahl_pdf.php
+++ app/Views/pdf/invoice_mitra_ahl_pdf.php
@@ -97,8 +97,8 @@
                 <tr>
                     <th scope="col" style="text-transform: capitalize; text-align:center; "><?= $upah_ahl["nama_lengkap"]  ?></th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Rp. <?= number_format($upah_ahl["upah_mitra"])   ?> </th>
-                    <th scope="col" style="text-transform: capitalize; text-align:center; ">Rp. <?= number_format($upah_ahl["insentif"])   ?> </th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Rp. <?= number_format($upah_ahl["bonus_kehadiran"])   ?> </th>
+                    <th scope="col" style="text-transform: capitalize; text-align:center; ">Rp. <?= number_format($upah_ahl["insentif"])   ?> </th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Rp. <?= number_format($upah_ahl["booster_penugasan"])   ?> </th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Rp. <?= number_format($upah_ahl["model_class"])   ?> </th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Rp. <?= number_format($upah_ahl["penalangan"])   ?> </th>
