f85f534c387d9bb7e44ac9e47086b3c2783acfb7
Chan Chan Bayu Bahari <63919358+chanchanbayub@users.noreply.github.com>
2026-08-05 11:02:39 +0700

    Update invoice_mitra_ahl_pdf.php
    merubah upah mitra menjadi gaji pokok

--- app/Views/pdf/invoice_mitra_ahl_pdf.php
+++ app/Views/pdf/invoice_mitra_ahl_pdf.php
@@ -80,7 +80,7 @@
             <thead>
                 <tr>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Mitra Pengajar</th>
-                    <th scope="col" style="text-transform: capitalize; text-align:center; ">Upah Mitra </th>
+                    <th scope="col" style="text-transform: capitalize; text-align:center; ">Gaji Pokok </th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Bonus Kehadiran</th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Insentif/Backup</th>
                     <th scope="col" style="text-transform: capitalize; text-align:center; ">Booster Penugasan</th>
